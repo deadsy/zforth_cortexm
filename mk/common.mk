92539e938ca6bfa2a3ecaf7a19d5b681d6a45711
Jason Harris <sirmanlypowers@gmail.com>
2020-08-05 08:23:56 -0700

    Direct calling of the linker.

--- mk/common.mk
+++ mk/common.mk
@@ -6,8 +6,8 @@ HOST_GCC = gcc
 XTOOLS_DIR = $(TOP)/xtools
 
 # library paths
-X_LIBGCC_DIR = $(XTOOLS_DIR)/lib/gcc/arm-none-eabi/9.2.1/arm/v5te/hard
-X_LIBC_DIR = $(XTOOLS_DIR)/arm-none-eabi/lib/arm/v5te/hard
+X_LIBGCC_DIR = $(XTOOLS_DIR)/lib/gcc/arm-none-eabi/9.2.1/thumb/v7e-m+fp/hard
+X_LIBC_DIR = $(XTOOLS_DIR)/arm-none-eabi/lib/thumb/v7e-m+fp/hard
 
 # tools
 X_GCC = $(XTOOLS_DIR)/bin/arm-none-eabi-gcc
